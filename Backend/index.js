e84c8b2834376085bcf0bf5328e2cfc0997fd0ee
purvesh2006 <purvesh.neve24@spit.ac.in>
2025-09-20 15:15:33 +0530

    Add files via upload

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,58 @@
 import express from "express";
+import cors from "cors";
+import pg from "pg";
 
 const app = express();
-const PORT = 5000;
-
+app.use(cors());
 app.use(express.json());
+const port = 5000;
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "workerRegister",
+  password: "Purvesh06$$",
+  port: 5432,
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+db.connect();   
+
+app.post("/api/worker/workerId", async (req, res) => {
+  const { workerId } = req.body; // Get workerId from request body
+  try {
+    const result = await db.query('SELECT * FROM details WHERE workerId = $1', [workerId]);
+    if (result.rows.length > 0) {
+      const worker = result.rows[0];
+      res.json({ success: true, workerId: worker.workerId, name: worker.name, age: worker.age, phone: worker.phone });
+    } else {
+      res.json({ success: false, message: "Worker not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
 });
+
+app.post("/api/worker/register", async (req, res) => {
+  const { name, location, gender ,age, phone, password } = req.body;
+  try {
+    const check = await db.query('SELECT * FROM wregister WHERE name = $1', [name]);
+
+    if(check.rows.length > 0){
+      res.send({ success: false, message: "Worker already registered!! Please Login" });
+    }
+    else{
+      const insertQuery = await db.query('INSERT INTO wregister (name, location,gender ,age, phone, password) VALUES ($1, $2, $3, $4, $5, $6)', [name, location,gender, age, phone, password]);
+      res.send({ success: true, message: "Worker registered successfully" });
+    }
+}
+catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, message: "Server error" });
+}});
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
